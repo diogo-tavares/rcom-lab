2946fbf903a9e709d11fd60fe0582efb5884f48b
danielcb7 <danielcb7@hotmail.com>
2024-04-30 12:11:06 +0100

    llread

--- link_library.c
+++ link_library.c
@@ -378,9 +378,79 @@ int llwrite(int fd, char * buffer, int length){
 
 }
 
-int llread(int fd, char * buffer){
+int llread(int fd, char * buffer)
+{
     // read character by character until flag is encountered
+    int STOP=FALSE;
+    int res;
+    char state=START;
 
+    const char flag = 0x5c;
+	const char transmitter_address = 0x03;
+	const char receiver_address = 0x01;
+	const char control_set = 0x08;
+	const char control_ua = 0x06;
+
+    char recv[32];
+    int recv_i=0;
+    char received_byte;
+ 
+
+    while (STOP==FALSE) {  
+        printf("Reading\n");     /* loop for input */
+        res = read(fd,&received_byte,1); 
+        recv[recv_i] = received_byte;
+        recv_i++;
+
+        printf("LLREAD -> State: %d, Byte: %x, Res: %d\n, ", state, received_byte, res);
+        //UA state machine
+        switch (state){
+            case START:
+                if (received_byte == flag){
+                    state = FLAG_RCV;
+                    printf("State: %d, Byte: %x\n", state, received_byte);
+                }
+                    
+                break;
+ 
+            case FLAG_RCV:
+                if (received_byte == receiver_address)
+                    state = A_RCV;
+                else{
+                    state = START;
+                    recv_i = 0;
+                }
+                break;
+ 
+            case A_RCV:
+                if (received_byte == control_set)
+                    state = C_RCV;
+                else{
+                    state = START;
+                    recv_i = 0;
+                }
+                break;
+ 
+            case C_RCV:
+                if (received_byte == (recv[1]) ^ recv[2])
+                    state = BCC_OK;
+                else{
+                    state = START;
+                    recv_i = 0;
+                }
+                break;
+ 
+            case BCC_OK:
+                if (received_byte == flag){
+                    //state = STOP_STATE;
+                    STOP = TRUE;
+				}
+                break;
+        
+            //case STOP_STATE:
+            break;
+        }
+    }
 }
 
 int byte_stuffing(char *buf, char *newbuff)
